906d9727ea99e329632381a5ae96a8da213071dd
Tristin Smith <tsmithassoc@gmail.com>
2024-01-15 19:46:19 -0500

    Added more labels, buttons, and a slider. Connected slider to view controller. Adjusted the hit me message to print the sliders current location.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -8,25 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentValue: Int = 50
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     @IBAction func showAlert(){
+        let message = "The value of the slider is: \(currentValue)"
+        
         let alert = UIAlertController(
             title: "Hello, World",
-            message: "This is my first app!",
+            message: message,
             preferredStyle: .alert)
         
         let action = UIAlertAction(
-            title: "Awesome",
+            title: "OK",
             style: .default,
             handler: nil)
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
     }
 }
 
